fef02248cbefbace2012ed3789c7d68caffaea31
Todd Bryant <hello@toddaas.com>
2021-01-24 18:27:01 +1100

    added forms

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from config import Config
+from flask import render_template
+from forms import UploadForm
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "index"
+    return 'index'
 
-@app.route("/upload")
-def hello():
-    return "Hello World!"
-
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    form = UploadForm()
+    return render_template('uploader.html', form=form)
---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileRequired
+
+class UploadForm(FlaskForm):
+    afile = FileField(validators=[FileRequired()])
+    submit = SubmitField('Upload')
