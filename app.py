715022bdea58e2b8cdcb8aadcb3fae43d7755fa7
Todd Bryant <hello@toddaas.com>
2021-01-27 22:24:26 +1100

    fixed ext in filename

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def uploader():
     if form.validate_on_submit():
         f = form.file.data
         filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],'date_goes_here' + os.path.splitext(filename)[1]))
     return render_template('uploader.html', form=form)
